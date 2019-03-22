b37ba31f1ddb0dddb92505650db9bab7dbbeb199
Norby Baruani <norbybaru@gmail.com>
2019-03-22 12:08:53 +0200

    change repo name and fix push script

--- hooks/vars.sh
+++ hooks/vars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export VARS='
-REPO_NAME=norby/php BASE_IMAGE=fpm-alpine VERSION=7.1 EXTRA_TAGS="7.1-fpm-alpine"
-REPO_NAME=norby/php BASE_IMAGE=fpm-alpine VERSION=7.2 EXTRA_TAGS="latest-fpm-alpine;7.2-fpm-alpine"
+REPO_NAME=norby/php-alpine BASE_IMAGE=fpm-alpine VERSION=7.1 EXTRA_TAGS="7.1-fpm"
+REPO_NAME=norby/php-alpine BASE_IMAGE=fpm-alpine VERSION=7.2 EXTRA_TAGS="latest-fpm;7.2-fpm"
 '
