9a5370538b006ce4b519e15abb1a4ce8e15ea098
Norby Baruani <norby.baruani@gmail.com>
2021-01-19 21:40:45 +0200

    Add php-7.4

--- hooks/vars.sh
+++ hooks/vars.sh
@@ -5,6 +5,8 @@ REPO_NAME=norby/php-alpine BASE_IMAGE=fpm-alpine VERSION=7.1 EXTRA_TAGS="7.1-fpm
 REPO_NAME=norby/php-alpine BASE_IMAGE=cli-apline VERSION=7.1 EXTRA_TAGS="7.1-cli"
 REPO_NAME=norby/php-alpine BASE_IMAGE=fpm-alpine VERSION=7.2 EXTRA_TAGS="7.2-fpm"
 REPO_NAME=norby/php-alpine BASE_IMAGE=cli-alpine VERSION=7.2 EXTRA_TAGS="7.2-cli"
-REPO_NAME=norby/php-alpine BASE_IMAGE=fpm-alpine VERSION=7.3 EXTRA_TAGS="latest-fpm;7.3-fpm"
-REPO_NAME=norby/php-alpine BASE_IMAGE=cli-alpine VERSION=7.3 EXTRA_TAGS="latest-cli;7.3-cli"
+REPO_NAME=norby/php-alpine BASE_IMAGE=fpm-alpine VERSION=7.3 EXTRA_TAGS="7.3-fpm"
+REPO_NAME=norby/php-alpine BASE_IMAGE=cli-alpine VERSION=7.3 EXTRA_TAGS="7.3-cli"
+REPO_NAME=norby/php-alpine BASE_IMAGE=fpm-alpine VERSION=7.4 EXTRA_TAGS="latest-fpm;7.4-fpm"
+REPO_NAME=norby/php-alpine BASE_IMAGE=cli-alpine VERSION=7.4 EXTRA_TAGS="latest-cli;7.4-cli"
 '
