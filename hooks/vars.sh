abeba21c089737252e3a9b6ed7ed53cc0811e088
Norby Baruani <norby.baruani@gmail.com>
2021-01-20 13:33:13 +0200

    fix base image typo

--- hooks/vars.sh
+++ hooks/vars.sh
@@ -2,7 +2,7 @@
 
 export VARS='
 REPO_NAME=norby/php-alpine BASE_IMAGE=fpm-alpine VERSION=7.1 EXTRA_TAGS="7.1-fpm"
-REPO_NAME=norby/php-alpine BASE_IMAGE=cli-apline VERSION=7.1 EXTRA_TAGS="7.1-cli"
+REPO_NAME=norby/php-alpine BASE_IMAGE=cli-alpine VERSION=7.1 EXTRA_TAGS="7.1-cli"
 REPO_NAME=norby/php-alpine BASE_IMAGE=fpm-alpine VERSION=7.2 EXTRA_TAGS="7.2-fpm"
 REPO_NAME=norby/php-alpine BASE_IMAGE=cli-alpine VERSION=7.2 EXTRA_TAGS="7.2-cli"
 REPO_NAME=norby/php-alpine BASE_IMAGE=fpm-alpine VERSION=7.3 EXTRA_TAGS="7.3-fpm"
