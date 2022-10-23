bb4aee569225703042462036e53f8c4e580544de
Norby Baruani <norby.baruani@gmail.com>
2022-10-23 14:58:57 +0200

    php8 and php81 docker build

--- hooks/vars.sh
+++ hooks/vars.sh
@@ -7,6 +7,10 @@ REPO_NAME=norby/php-alpine BASE_IMAGE=fpm-alpine VERSION=7.2 EXTRA_TAGS="7.2-fpm
 REPO_NAME=norby/php-alpine BASE_IMAGE=cli-alpine VERSION=7.2 EXTRA_TAGS="7.2-cli"
 REPO_NAME=norby/php-alpine BASE_IMAGE=fpm-alpine VERSION=7.3 EXTRA_TAGS="7.3-fpm"
 REPO_NAME=norby/php-alpine BASE_IMAGE=cli-alpine VERSION=7.3 EXTRA_TAGS="7.3-cli"
-REPO_NAME=norby/php-alpine BASE_IMAGE=fpm-alpine VERSION=7.4 EXTRA_TAGS="latest-fpm;7.4-fpm"
-REPO_NAME=norby/php-alpine BASE_IMAGE=cli-alpine VERSION=7.4 EXTRA_TAGS="latest-cli;7.4-cli"
+REPO_NAME=norby/php-alpine BASE_IMAGE=fpm-alpine VERSION=7.4 EXTRA_TAGS="7.4-fpm"
+REPO_NAME=norby/php-alpine BASE_IMAGE=cli-alpine VERSION=7.4 EXTRA_TAGS="7.4-cli"
+REPO_NAME=norby/php-alpine BASE_IMAGE=fpm-alpine VERSION=8.0 EXTRA_TAGS="8.0-fpm"
+REPO_NAME=norby/php-alpine BASE_IMAGE=cli-alpine VERSION=8.0 EXTRA_TAGS="8.0-cli"
+REPO_NAME=norby/php-alpine BASE_IMAGE=fpm-alpine VERSION=8.1 EXTRA_TAGS="latest-fpm;8.1-fpm"
+REPO_NAME=norby/php-alpine BASE_IMAGE=cli-alpine VERSION=8.1 EXTRA_TAGS="latest-cli;8.1-cli"
 '
